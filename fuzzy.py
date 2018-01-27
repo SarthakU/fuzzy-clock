6f09f7a9a41deaab88f766a2867153e0e04ab075
Sarthak Upadhyay <sarthak7u@gmail.com>
2018-01-27 19:15:29 +0530

    :lipstick: Capitalize output

--- fuzzy.py
+++ fuzzy.py
@@ -2,17 +2,17 @@ import datetime
 import random
 
 TENS_DICT = [
-    '', '', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy',
-    'eighty', 'ninety'
+    '', '', 'Twenty', 'Thirty', 'Fourty', 'Fifty', 'Sixty', 'Seventy',
+    'Eighty', 'Ninety'
 ]
 
 ONES_DICT = [
-    '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'
+    '', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'
 ]
 
 TEENS = [
-    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
-    'seventeen', 'eighteen', 'nineteen'
+    'Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen',
+    'Seventeen', 'Eighteen', 'Nineteen'
 ]
 
 
@@ -38,22 +38,23 @@ def fuzzyTime(hour, minutes):
     hour = hour % 12
     if minutes > 33:
         hour = next(hour)
-    print(cond)
     if cond == 0:
         return n2a(hour) + ' o\'clock'
     elif cond == 3:
-        return 'quarter' + ' past ' + n2a(hour)
+        return 'Quarter' + ' past ' + n2a(hour)
     elif cond == 6:
-        return 'half' + ' past ' + n2a(hour)
+        return 'Half' + ' past ' + n2a(hour)
     elif cond == 9:
-        return 'quarter' + ' to ' + n2a(hour)
+        return 'Quarter' + ' to ' + n2a(hour)
     elif 0 < cond < 6:
         return n2a(cond * 5) + ' past ' + n2a(hour)
     else:
         return n2a(60 - cond * 5) + ' to ' + n2a(hour)
 
 
+# # Get fuzzy time for current time :
+#
 # time = datetime.datetime.now().timetuple()
 # hour = time[3]
 # minutes = time[4]
-# fuzzyTime(hour, minutes)
+# print(fuzzyTime(hour, minutes))
