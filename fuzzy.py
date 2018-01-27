7f430c4090b2c1ff997a6a424e42b341c1d8a5c6
Sarthak Upadhyay <sarthak7u@gmail.com>
2018-01-27 16:28:54 +0530

    Init

---
+++ fuzzy.py
@@ -0,0 +1,61 @@
+import datetime
+import random
+
+TENS_DICT = [
+    '', '', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy',
+    'eighty', 'ninety'
+]
+
+ONES_DICT = [
+    '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'
+]
+
+TEENS = [
+    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+    'seventeen', 'eighteen', 'nineteen'
+]
+
+
+def n2a(number):
+    if number == 0:
+        return 'zero'
+    elif number < 10:
+        return ONES_DICT[number % 10]
+    elif number < 20 and number > 9:
+        return TEENS[number - 10]
+    elif number % 10 == 0:
+        return TENS_DICT[number // 10]
+    else:
+        return TENS_DICT[number // 10] + ' ' + ONES_DICT[number % 10]
+
+
+
+
+time = datetime.datetime.now().timetuple()
+print(time)
+hour = time[3]
+minutes = time[4]
+
+
+def next(n):
+    return 12 if n == 11 else n + 1
+
+
+def fuzzyTime(hour, minutes):
+    cond = ((minutes + 2) % 60) // 5
+    hour = hour % 12
+    if cond == 0:
+        return n2a(hour) + ' o\'clock'
+    elif cond == 3:
+        return 'quarter' + ' past ' + n2a(hour)
+    elif cond == 6:
+        return 'half' + ' past ' + n2a(hour)
+    elif cond == 9:
+        return 'quarter' + ' to ' + n2a(next(hour))
+    elif 0 < cond < 6:
+        return n2a(cond * 5) + ' past ' + n2a(hour)
+    else:
+        return n2a(60 - cond * 5) + ' to ' + n2a(next(hour))
+
+
+print(fuzzyTime(hour, minutes))
